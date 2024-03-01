5e0978d4d6e98a592701a04f4bb5f78852aff1bb
Pelin Dayıoğlu <82973830+pelinda@users.noreply.github.com>
2024-03-01 11:43:20 +0300

    Classes
    Java Class

---
+++ src/classes/Main.java
@@ -0,0 +1,19 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//reference type
+		CustomerManager customerManager=new CustomerManager();
+		CustomerManager customerManager2=new CustomerManager();
+		customerManager=customerManager2;
+		
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+		
+	}
+
+}
+
